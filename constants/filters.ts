07d48973e0a249036ab37e89900f439eae941c88
TeddRod <rogers.teddy@proton.me>
2023-08-30 16:25:06 +0200

    fix: seed data keywords

--- constants/filters.ts
+++ constants/filters.ts
@@ -1,4 +1,4 @@
 const invalidStringsCast =
-  'a an to this that of for when with what and while is not the are or which whole do then the on in any your t but get only . ,';
+  'devant sur sous à la le les il ils elle elles mais si on je ne vous tu nous de du ce se ça sa pour avec en pas qui au aux ou où et donc or ni car hors tout toute toutes tous un unes unes assez mon ma mes des être ô cet cette also all my whom you be well so has i not  have a an to this its that of for when with what who and while is it not the are or which whole do then the on in any your t but get only . ,';
 
 export const invalidStrings: string[] = invalidStringsCast.split(' ');
